7742ccc4f7b678e01ff87549450bce5878abec2f
Denner Robert <dennerobert@hotmail.com>
2023-02-13 19:40:27 -0300

    Saída estoque, view list, form e template.

--- apps/estoque_produtos/urls.py
+++ apps/estoque_produtos/urls.py
@@ -8,6 +8,9 @@ app_name = 'estoque_produtos'
 urlpatterns = [
     path('', views.EstoqueEntradaList.as_view(), name='estoque_entrada_list'),
     path('add/', views.add_estoque.as_view(), name='estoque_entrada_add'),
+    path('list/', views.EstoqueSaidaList.as_view(), name='estoque_saida_list'),
+    path('saida/', views.del_estoque.as_view(), name='estoque_saida'),
+
     path('<int:pk>/up/', views.up_estoque.as_view(), name='estoque_entrada_up'),
 
     path('get_saldo_ajax/', get_saldo_ajax, name='get_saldo_ajax'),
--- apps/estoque_produtos/views.py
+++ apps/estoque_produtos/views.py
@@ -49,21 +49,54 @@ class up_estoque(UpdateView):
 
     def form_valid(self, form):
         estoque = form.save()
-
         estoque_itens_formset = EstoqueItensFormSet(self.request.POST, instance=self.object)
         if estoque_itens_formset.is_valid():
             for estoque_item_form in estoque_itens_formset:
                 estoque_itens_formset = estoque_item_form.save(commit=False)
                 total_produto = Produtos.objects.filter(id = estoque_itens_formset.produto_id).first()
                 if total_produto:
-                    total_produto.estoque +=  estoque_itens_formset.quantidade
+                    if estoque.movimentacao == 'e':
+                        total_produto.estoque +=  estoque_itens_formset.quantidade
+                    else:
+                        total_produto.estoque -=  estoque_itens_formset.quantidade
                     total_produto.save()
 
                 estoque_itens_formset.save()
+            if estoque.movimentacao == 'e':
+                return redirect('estoque_produtos:estoque_entrada_list')
+            else:
+                return redirect('estoque_produtos:estoque_saida_list')
 
+        if estoque.movimentacao == 'e':
             return redirect('estoque_produtos:estoque_entrada_list')
-        
-        return redirect('estoque_produtos:estoque_entrada_list')
+        else:
+            return redirect('estoque_produtos:estoque_saida_list')
+
+
+class EstoqueSaidaList(ListView):
+    raise_exception = True
+    model = Estoque
+    template_name = 'estoque_saida_list.html'
+
+    def get_queryset(self, **kwargs):
+        return Estoque.objects.filter(movimentacao='s')   
+    
+
+class del_estoque(CreateView):
+    template_name ='estoque_entrada_form.html'
+    model = Estoque
+    form_class = EstoqueForm
+    success_message = 'Produtos retirado com sucesso'
+
+    def get_context_data(self, **kwargs):
+        ctx = super(del_estoque, self).get_context_data(**kwargs)
+        return ctx
+
+    def form_valid(self, form):
+        estoque = form.save(commit=False)
+        estoque.movimentacao = 's'
+        estoque.save()
+        return redirect('estoque_produtos:estoque_entrada_up', pk=estoque.pk)
     
 
 def get_saldo_ajax(request):
