767df9baea686fb53e1a93563dc531e76c7c5d9c
Denner Robert <dennerobert@hotmail.com>
2024-06-27 20:10:18 -0300

    Ajuste tela de login

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,13 +1,7 @@
-from django.conf.urls import url
-from django.urls import path, include
-from django.contrib import admin
-from apps.core.views import CadastroUsuarioView, index
+from django.urls import path
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-     # path('', index, name="index"),
-
     path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('register/', CadastroUsuarioView.as_view(), name='register'),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,17 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
-from django.urls import reverse_lazy
-from django.views import generic
-
-from apps.core.forms import CadastroUsuarioForm
-
 
 def index(request):
     return render(request, 'index.html')
-
-
-class CadastroUsuarioView(generic.CreateView):
-    model = User
-    template_name = 'register.html'
-    form_class = CadastroUsuarioForm
-    success_url = reverse_lazy('login')
