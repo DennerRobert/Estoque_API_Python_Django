6edaf425b5efd7bb710d0a8bdd1a176bb419d4a1
Denner Robert <dennerobert@hotmail.com>
2024-07-05 02:26:24 -0300

    Ajustes e correções em estoque entrada. E pequenas  alterações de layout gerais.

--- apps/estoque_produtos/forms.py
+++ apps/estoque_produtos/forms.py
@@ -3,22 +3,30 @@ from .models import Estoque, EstoqueItens
 from django.forms import formset_factory
 
 class EstoqueForm(forms.ModelForm):
-    class Meta:
-        model = Estoque
-        filds = ('funcionario', 'nf', 'movimentacao')
-        exclude = ('id',)
+	class Meta:
+		model = Estoque
+		fields = ('funcionario', 'nf', 'movimentacao')
+		exclude = ('id', 'movimentacao','funcionario')
+		widgets = {
+			'nf': forms.TextInput(attrs={'placeholder':'99999999'}),
+			'funcionario':forms.TextInput(attrs={'required':'False'})
+		}
 
 class EstoqueItensForm(forms.ModelForm):
-    class Meta:
-        model: EstoqueItens
-        filds = ('produto', 'quantidade', 'saldo')
-        exclude = ('id', 'estoque',)
+	class Meta:
+		model: EstoqueItens
+		filds = ('produto', 'quantidade', 'saldo')
+		exclude = ('id', 'estoque',)
+		widgets = {
+			'quantidade': forms.TextInput(attrs={'placeholder':'0','required': 'required'}),
+			'saldo': forms.TextInput(attrs={'placeholder':'0,00','required': 'required'}),
+		}
 
 EstoqueItensFormSet = forms.inlineformset_factory(
-    Estoque,
-    EstoqueItens,
-    EstoqueItensForm,
-    min_num=1,
-    extra=0,    
-    validate_min=True
+	Estoque,
+	EstoqueItens,
+	EstoqueItensForm,
+	min_num=1,
+	extra=0,    
+	validate_min=True
 )
--- apps/estoque_produtos/views.py
+++ apps/estoque_produtos/views.py
@@ -9,104 +9,109 @@ from django.http import JsonResponse
 
 
 class EstoqueEntradaList(ListView):
-    raise_exception = True
-    model = Estoque
-    template_name = 'estoque_entrada_list.html'
+	raise_exception = True
+	model = Estoque
+	template_name = 'estoque_entrada_list.html'
 
-    def get_queryset(self, **kwargs):
-        return Estoque.objects.filter(movimentacao='e')
+	def get_queryset(self, **kwargs):
+		return Estoque.objects.filter(movimentacao='e')
 
 
 class add_estoque(CreateView):
-    template_name ='estoque_entrada_form.html'
-    model = Estoque
-    form_class = EstoqueForm
-    success_message = 'Produtos adicionado com sucesso'
+	template_name ='estoque_entrada_form.html'
+	model = Estoque
+	form_class = EstoqueForm
+	success_message = 'Produtos adicionado com sucesso'
 
-    def get_context_data(self, **kwargs):
-        ctx = super(add_estoque, self).get_context_data(**kwargs)
-        return ctx
+	def get_context_data(self, **kwargs):
+		ctx = super(add_estoque, self).get_context_data(**kwargs)
 
-    def form_valid(self, form):
-        estoque = form.save(commit=False)
-        estoque.movimentacao = 'e'
-        estoque.save()
-        return redirect('estoque_produtos:estoque_entrada_up', pk=estoque.pk)
+		return ctx
 
+	def form_valid(self, form):
+		if form.is_valid():
+			estoque = form.save(commit=False)
+			print('Successfully', )
+			estoque.movimentacao = 'e'
+			estoque.funcionario = self.request.user
+			estoque.save()
+			return redirect('estoque_produtos:estoque_entrada_up', pk=estoque.pk)
+		else:
+			print('Error', self.request.user)
 
 class up_estoque(UpdateView):
-    template_name ='estoque_entrada_form.html'
-    model = Estoque
-    form_class = EstoqueForm
-    success_message = 'Produtos adicionado com sucesso'
+	template_name ='estoque_entrada_form.html'
+	model = Estoque
+	form_class = EstoqueForm
+	success_message = 'Produtos adicionado com sucesso'
 
-    def get_context_data(self, **kwargs):
-        ctx = super(up_estoque, self).get_context_data(**kwargs)
-        ctx['estoque_itens_formset'] = EstoqueItensFormSet(instance=self.object)
-        return ctx
+	def get_context_data(self, **kwargs):
+		ctx = super(up_estoque, self).get_context_data(**kwargs)
+		ctx['estoque_itens_formset'] = EstoqueItensFormSet(instance=self.object)
+		return ctx
 
-    def form_valid(self, form):
-        estoque = form.save()
-        estoque_itens_formset = EstoqueItensFormSet(self.request.POST, instance=self.object)
-        if estoque_itens_formset.is_valid():
-            for estoque_item_form in estoque_itens_formset:
-                estoque_itens_formset = estoque_item_form.save(commit=False)
-                total_produto = Produtos.objects.filter(id = estoque_itens_formset.produto_id).first()
-                if total_produto:
-                    if estoque.movimentacao == 'e':
-                        total_produto.estoque +=  estoque_itens_formset.quantidade
-                    else:
-                        total_produto.estoque -=  estoque_itens_formset.quantidade
-                    total_produto.save()
+	def form_valid(self, form):
+		estoque = form.save()
+		estoque_itens_formset = EstoqueItensFormSet(self.request.POST, instance=self.object)
+		
+		if estoque_itens_formset.is_valid():
+			for estoque_item_form in estoque_itens_formset:
+				estoque_itens_formset = estoque_item_form.save(commit=False)
+				total_produto = Produtos.objects.filter(id = estoque_itens_formset.produto_id).first()
+				
+				if total_produto:
+					if estoque.movimentacao == 'e':
+						total_produto.estoque +=  estoque_itens_formset.quantidade
+					else:
+						total_produto.estoque -=  estoque_itens_formset.quantidade
+					total_produto.save()
 
-                estoque_itens_formset.save()
-            if estoque.movimentacao == 'e':
-                return redirect('estoque_produtos:estoque_entrada_list')
-            else:
-                return redirect('estoque_produtos:estoque_saida_list')
+				estoque_itens_formset.save()
+			
+			if estoque.movimentacao == 'e':
+				return redirect('estoque_produtos:estoque_entrada_list')
+			else:
+				return redirect('estoque_produtos:estoque_saida_list')
 
-        if estoque.movimentacao == 'e':
-            return redirect('estoque_produtos:estoque_entrada_list')
-        else:
-            return redirect('estoque_produtos:estoque_saida_list')
-
+		if estoque.movimentacao == 'e':
+			return redirect('estoque_produtos:estoque_entrada_list')
+		else:
+			return redirect('estoque_produtos:estoque_saida_list')
 
 class EstoqueSaidaList(ListView):
-    raise_exception = True
-    model = Estoque
-    template_name = 'estoque_saida_list.html'
+	raise_exception = True
+	model = Estoque
+	template_name = 'estoque_saida_list.html'
 
-    def get_queryset(self, **kwargs):
-        return Estoque.objects.filter(movimentacao='s')   
-    
+	def get_queryset(self, **kwargs):
+		return Estoque.objects.filter(movimentacao='s')   
 
 class del_estoque(CreateView):
-    template_name ='estoque_entrada_form.html'
-    model = Estoque
-    form_class = EstoqueForm
-    success_message = 'Produtos retirado com sucesso'
+	template_name ='estoque_entrada_form.html'
+	model = Estoque
+	form_class = EstoqueForm
+	success_message = 'Produtos retirado com sucesso'
 
-    def get_context_data(self, **kwargs):
-        ctx = super(del_estoque, self).get_context_data(**kwargs)
-        return ctx
+	def get_context_data(self, **kwargs):
+		ctx = super(del_estoque, self).get_context_data(**kwargs)
+		return ctx
 
-    def form_valid(self, form):
-        estoque = form.save(commit=False)
-        estoque.movimentacao = 's'
-        estoque.save()
-        return redirect('estoque_produtos:estoque_entrada_up', pk=estoque.pk)
-    
-
+	def form_valid(self, form):
+		estoque = form.save(commit=False)
+		estoque.movimentacao = 's'
+		estoque.save()
+		return redirect('estoque_produtos:estoque_entrada_up', pk=estoque.pk)
+	
 def get_saldo_ajax(request):
-    # retomar att saldo
-    produto_id = request.GET.get('produto')
-    quantidade = request.GET.get('quantidade')
+		# retomar att saldo
+		produto_id = request.GET.get('produto')
+		quantidade = request.GET.get('quantidade')
 
-    if not quantidade:
-        quantidade = 0
-        
-    produto = Produtos.objects.filter(id = produto_id).first()
-    if produto:
-        saldo = int(produto.estoque) + int(quantidade)
+		if not quantidade:
+			quantidade = 0
+			
+		produto = Produtos.objects.filter(id = produto_id).first()
+		if produto:
+			saldo = int(produto.estoque) + int(quantidade)
 
-    return JsonResponse({'result': saldo})
+		return JsonResponse({'result': saldo})
