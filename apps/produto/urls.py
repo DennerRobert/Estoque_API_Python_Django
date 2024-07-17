2fa11738a1bdf26e1ca1b4f897443a0fc2272ed4
Denner Robert <dennerobert@hotmail.com>
2024-07-16 22:39:17 -0300

    Opção de imprimir as listagens.

--- apps/produto/urls.py
+++ apps/produto/urls.py
@@ -8,14 +8,17 @@ from .views import ProdutoViewSet
 app_name = 'produto'
 
 urlpatterns = [
-    path('', views.ProdutoListView.as_view(), name='produto_list'),
-    path('add/', views.ProdutoAddView.as_view(), name='produto_add'),
-    path('<int:pk>/edit/', views.ProdutoEditView.as_view(), name='produto_edit'),
-    path('<int:pk>/del/', views.DispostivosDeleteAjax.as_view(), name='produto_delete'),
+	path('', views.ProdutoListView.as_view(), name='produto_list'),
+	path('add/', views.ProdutoAddView.as_view(), name='produto_add'),
+	path('<int:pk>/edit/', views.ProdutoEditView.as_view(), name='produto_edit'),
+	path('<int:pk>/del/', views.DispostivosDeleteAjax.as_view(), name='produto_delete'),
 
-    # api produto
-    path('listar_produto/', ProdutoViewSet.as_view({'get': 'list'}), name='listar_produto'),
-    path('adicionar_produto/', ProdutoViewSet.as_view({'post': 'create'}), name='adicionar_produto'),
-    path('editar_produto/<int:pk>/', ProdutoViewSet.as_view({'patch': 'update'}), name='editar_produto'),
-    path('deletar_produto/<int:pk>/', ProdutoViewSet.as_view({'delete': 'destroy'}), name='deletar_produto'),
+	path('produtos/imprimir_atual/', views.ProdutoPrintAtualView.as_view(), name='produto_print_atual'),
+	path('produtos/imprimir_todos/', views.ProdutoPrintTodosView.as_view(), name='produto_print_todos'),
+
+	# api produto
+	path('listar_produto/', ProdutoViewSet.as_view({'get': 'list'}), name='listar_produto'),
+	path('adicionar_produto/', ProdutoViewSet.as_view({'post': 'create'}), name='adicionar_produto'),
+	path('editar_produto/<int:pk>/', ProdutoViewSet.as_view({'patch': 'update'}), name='editar_produto'),
+	path('deletar_produto/<int:pk>/', ProdutoViewSet.as_view({'delete': 'destroy'}), name='deletar_produto'),
 ]
--- apps/produto/views.py
+++ apps/produto/views.py
@@ -24,6 +24,9 @@ class ProdutoListView(ListView):
 		ctx = super(ProdutoListView, self).get_context_data(**kwargs)
 		ctx['query'] = self.request.GET.get('q', '')
 
+		for produto in ctx['produtos']:
+			produto.valor_total = produto.estoque * produto.preco
+
 		produtos = Produtos.objects.filter(ativo=True)
 		paginator = Paginator(produtos, self.paginate_by)
 		page_number = self.request.GET.get('page')
@@ -41,92 +44,129 @@ class ProdutoListView(ListView):
 
 
 class ProdutoAddView(CreateView):
-    template_name ='produtos_form.html'
-    model = Produtos
-    form_class = ProdutosForm
-    success_message = 'Produtos adicionado com sucesso'
+	template_name ='produtos_form.html'
+	model = Produtos
+	form_class = ProdutosForm
+	success_message = 'Produtos adicionado com sucesso'
 
-    def get_context_data(self, **kwargs):
-        ctx = super(ProdutoAddView, self).get_context_data(**kwargs)
-        ctx['title'] = 'Product Registration'
-        return ctx
+	def get_context_data(self, **kwargs):
+		ctx = super(ProdutoAddView, self).get_context_data(**kwargs)
+		ctx['title'] = 'Product Registration'
+		return ctx
 
-    def form_valid(self, form):
-        produtos_form = form.save(commit=False)
-        produtos_form.ativo = True
-        produtos_form.produto = produtos_form.produto
-        produtos_form.preco = produtos_form.preco
-        produtos_form.estoque = 0
-        produtos_form.save()
-        
-        return redirect('produto:produto_list')
+	def form_valid(self, form):
+		produtos_form = form.save(commit=False)
+		produtos_form.ativo = True
+		produtos_form.produto = produtos_form.produto
+		produtos_form.preco = produtos_form.preco
+		produtos_form.estoque = 0
+		produtos_form.save()
+		
+		return redirect('produto:produto_list')
 
 class ProdutoEditView(UpdateView):
-    model = Produtos
-    template_name = 'produtos_form.html'
-    form_class = ProdutosForm
+	model = Produtos
+	template_name = 'produtos_form.html'
+	form_class = ProdutosForm
 
-    def get_context_data(self, **kwargs):
-        ctx = super().get_context_data(**kwargs)
-        ctx['title'] = 'Edit Product'
-        return ctx
-    
-    def form_valid(self, form):
-        produto = form.save()
-        return redirect('produto:produto_list')
-    
+	def get_context_data(self, **kwargs):
+		ctx = super().get_context_data(**kwargs)
+		ctx['title'] = 'Edit Product'
+		return ctx
+	
+	def form_valid(self, form):
+		produto = form.save()
+		return redirect('produto:produto_list')
+	
 
 class DispostivosDeleteAjax(View):
-    def get(self, request, pk):
-        remove_produto = Produtos.objects.filter(pk=pk).first()
+	def get(self, request, pk):
+		remove_produto = Produtos.objects.filter(pk=pk).first()
 
-        if remove_produto:
-            remove_produto.ativo = False
-            remove_produto.save()
-            messages.success(self.request, self.success_message)  
-        else:
-            messages.success(self.request, self.error_message) 
+		if remove_produto:
+			remove_produto.ativo = False
+			remove_produto.save()
+			messages.success(self.request, self.success_message)  
+		else:
+			messages.success(self.request, self.error_message) 
 
-        return redirect('produto:produto_list') 
-    
+		return redirect('produto:produto_list') 
+	
 
 class ProdutoViewSet(viewsets.ViewSet):
 
-    serializer_class = ProdutoSerializer
+	serializer_class = ProdutoSerializer
 
-    def list(self, request):
-        produtos = Produtos.objects.all()
-        serializer = ProdutoSerializer(produtos, many=True)
-        return Response(serializer.data)
+	def list(self, request):
+		produtos = Produtos.objects.all()
+		serializer = ProdutoSerializer(produtos, many=True)
+		return Response(serializer.data)
+	
+
+	def create(self, request):
+		serializer = ProdutoSerializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=201)
+		return Response(serializer.errors, status=400)
+	
+
+	def update(self, request, pk=None):
+		try:
+			produto = Produtos.objects.get(pk=pk)
+			serializer = ProdutoSerializer(produto, data=request.data)
+			if serializer.is_valid():
+				serializer.save()
+				# return Response(serializer.data, status=status.HTTP_200_OK)
+				return Response(serializer.data,  status=201)
+			else:
+				# return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+				return Response(serializer.errors, status=400)
+			
+		except produto.DoesNotExist:
+			return Response(status=400)
+		
+
+	def destroy(self, request, pk, format=None):
+		produto = Produtos.objects.get(pk=pk)
+		produto.ativo = False
+		produto.save()
+		# return Response(status=status.HTTP_204_NO_CONTENT)
+		return Response(status=201)
     
 
-    def create(self, request):
-        serializer = ProdutoSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
-    
+class ProdutoPrintAtualView(ListView):
+	model = Produtos
+	template_name = 'produto_print.html'
+	context_object_name = 'produtos'
+	paginate_by = 10
 
-    def update(self, request, pk=None):
-        try:
-            produto = Produtos.objects.get(pk=pk)
-            serializer = ProdutoSerializer(produto, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                # return Response(serializer.data, status=status.HTTP_200_OK)
-                return Response(serializer.data,  status=201)
-            else:
-                # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-                return Response(serializer.errors, status=400)
-            
-        except produto.DoesNotExist:
-            return Response(status=400)
-        
+	def get_queryset(self):
+		queryset = super().get_queryset()
+		# filtro com base na query atual
+		query = self.request.GET.get('q')
+		if query:
+			queryset = queryset.filter(produto__icontains=query)
+		return queryset
+	
+	def get_context_data(self, **kwargs):
+		ctx = super().get_context_data(**kwargs)
+		# cálculo do valor total do estoque no contexto
+		for produto in ctx['produtos']:
+			produto.valor_total = produto.estoque * produto.preco
 
-    def destroy(self, request, pk, format=None):
-        produto = Produtos.objects.get(pk=pk)
-        produto.ativo = False
-        produto.save()
-        # return Response(status=status.HTTP_204_NO_CONTENT)
-        return Response(status=201)
+		return ctx
+
+class ProdutoPrintTodosView(ListView):
+	model = Produtos
+	template_name = 'produto_print.html'
+	context_object_name = 'produtos'
+	paginate_by = None
+
+	def get_context_data(self, **kwargs):
+		ctx = super().get_context_data(**kwargs)
+
+		for produto in ctx['produtos']:
+			produto.valor_total = produto.estoque * produto.preco
+
+		return ctx
